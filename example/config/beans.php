d0d3f6dcc65d7f7f64387e0f10e571027b7f76c0
Yurun <admin@yurunsoft.com>
2022-08-15 16:01:42 +0800

    修复测试

--- example/config/beans.php
+++ example/config/beans.php
@@ -68,4 +68,7 @@ return [
             'TestProcess',
         ],
     ],
+    'ErrorLog' => [
+        'exceptionLevel' => \E_ERROR | \E_PARSE | \E_CORE_ERROR | \E_COMPILE_ERROR | \E_USER_ERROR | \E_RECOVERABLE_ERROR,
+    ],
 ];
