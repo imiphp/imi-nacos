e1ab81742a33b6768187a9043b152d6302aa892f
Yurun <admin@yurunsoft.com>
2022-08-15 15:27:00 +0800

    优化代码

--- example/Process/SwooleTestProcess.php
+++ example/Process/SwooleTestProcess.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace app\Process;
 
 use Imi\Config;
+use Imi\Event\Event;
 use Imi\Swoole\Process\Annotation\Process;
 use Imi\Swoole\Process\BaseProcess;
+use Imi\Util\ImiPriority;
 
 /**
  * @Process("TestProcess")
@@ -15,11 +17,15 @@ class SwooleTestProcess extends BaseProcess
 {
     public function run(\Swoole\Process $process): void
     {
+        $channel = new \Swoole\Coroutine\Channel();
+        Event::on('IMI.PROCESS.END', function () use ($channel) {
+            $channel->push(1);
+        }, ImiPriority::IMI_MAX);
         // @phpstan-ignore-next-line
-        while (true)
+        do
         {
             var_dump(__METHOD__, Config::get('nacos'));
-            sleep(3);
         }
+        while (!$channel->pop(3));
     }
 }
